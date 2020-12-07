6da17f835d1d021a2587cfd52e11b9bf9ab8a9bd
k.chukharev@mail.ru <k.chukharev@mail.ru>
2020-12-07 16:27:35 +0300

    Добавьте файлы проекта.

---
+++ Zad2(december1)/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad2_december1_
+{
+    class Class1
+    {
+        double a, b, c = 0;
+        public Class1(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        double Ploshad(double a, double b, double c)
+        {
+            double poluPerimetr = (a + b + c) / 2;
+            return Math.Sqrt(poluPerimetr * (poluPerimetr - a) * (poluPerimetr - b) * (poluPerimetr - c));
+        }
+
+        double Perimetr(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+
+        string Type(double a, double b, double c)
+        {
+            if (Math.Pow(c, 2) != Math.Pow(a, 2) + Math.Pow(b, 2)) return (Math.Pow(c, 2) < Math.Pow(a, 2) + Math.Pow(b, 2)) ? "Острый" : "Тупой";
+            else return "Прямоугольный";
+        }
+
+        public void trianglesCount(List<Class1> list)
+        {
+            int ostr = 0;
+            int pryam = 0;
+            int tup = 0;
+            string res = "";
+            foreach (Class1 triangle in list)
+            {
+                string type = Type(triangle.a, triangle.b, triangle.c);
+                switch (type)
+                {
+                    case "Прямоугольный":
+                        {
+                            pryam++;
+                            break;
+                        }
+                    case "Острый":
+                        {
+                            ostr++;
+                            break;
+                        }
+                    case "Тупой":
+                        {
+                            tup++;
+                            break;
+                        }
+                }
+            }
+            System.Windows.MessageBox.Show($"Острых {ostr}, прямоугольных {pryam}, тупых {tup}");
+        }
+    }
+}
---
+++ Zad2(december1)/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zad2_december1_
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Class1> list = new List<Class1>();
+            Class1 triangle1 = new Class1(10, 10, 10);
+            Class1 triangle2 = new Class1(3, 4, 5);
+            Class1 triangle3 = new Class1(4, 5, 7);
+            list.Add(triangle1);
+            list.Add(triangle2);
+            list.Add(triangle3);
+            triangle1.trianglesCount(list);
+        }
+    }
+}
